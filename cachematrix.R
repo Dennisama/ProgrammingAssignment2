ef0780593e8065c3de77060f3b96a0271f2961ba
Dennisama <35117372+Dennisama@users.noreply.github.com>
2018-01-19 19:30:07 -0500

    Codes have been added
    BiaoZhang, my codes for assignment2 have been added to the original R file (file's name dose not change).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creating a special "vector", which is a list containing set and get functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse<-NULL
+        set<-function(y) {
+              x<<-y
+        inverse<<-NULL  
+        }
+        get<-function() x
+        setinverse<-function(inv) inverse<<-inv
+        getinverse<-function() inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calulating the inverse of matrix, getting the result from the cache if necessary
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         inverse <- x$getinverse()
+         if(!is.null(inverse)){
+                    message("getting cached data")
+                    return(inverse)
+         }
+         result <- x$get()
+         inverse <- solve(result, ...)
+         x$setinverse(inverse)
+         inverse
 }
